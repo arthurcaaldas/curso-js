9e2e6933918f069c5a53081df05c7b56ec3b1b10
Danilo Rodrigues <dnrodriguesm@gmail.com>
2023-01-31 16:38:46 -0300

    alteracao

--- aula-100/script.js
+++ aula-100/script.js
@@ -4,3 +4,14 @@ Bem utilizada nos frameworks modernos;
 Porém não deve substituir as funtions por completo;
 */
 
+let consoleTeste = () => {
+  console.log("Olá mundo!");
+};
+
+consoleTeste();
+
+let soma = (a, b) => {
+  return a + b;
+};
+
+console.log(soma(10, 20));
